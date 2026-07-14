d2c701ce124f4a900941c84104eecc34d181473c
Himanshi Yadav <himanshiyadav1412004@gmail.com>
2026-07-14 10:52:16 +0530

    Update bookService.js

--- library-management-frontend/src/services/bookService.js
+++ library-management-frontend/src/services/bookService.js
@@ -1,19 +1,19 @@
 import api from "../api/axios";
 
-// Get all books
+
 export const getBooks = () => api.get("/books");
 
-// Get one book
+
 export const getBook = (bookId) => api.get(`/books/${bookId}`);
 
-// Add book
+
 export const addBook = (data) => api.post("/books", data);
 
-// Update book
+
 export const updateBook = (bookId, data) =>
     api.put(`/books/${bookId}`, data);
 
-// Delete book
+
 export const deleteBook = (bookId) =>
     api.delete(`/books/${bookId}`);
 
